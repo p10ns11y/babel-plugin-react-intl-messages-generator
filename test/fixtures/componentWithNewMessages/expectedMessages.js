c4cca3fdaaa831d5a2b974e513f3e19c487e1822
Peramanathan <sathyam.peram@gmail.com>
2017-02-12 20:15:16 +0100

    Add partially edited simulation

--- test/fixtures/componentWithNewMessages/expectedMessages.js
+++ test/fixtures/componentWithNewMessages/expectedMessages.js
@@ -1,12 +1,12 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  NameTheKey: {
-    id: 'test.fixtures.componentWithNewMessages.actualWithNew...',
+  helloTxt: {
+    id: 'test.fixtures.componentWithNewMessages.actualWithNew.text.hello',
     defaultMessage: 'Hello'
   },
-  NameTheKey: {
-    id: 'test.fixtures.componentWithNewMessages.actualWithNew...',
+  worldTxt: {
+    id: 'test.fixtures.componentWithNewMessages.actualWithNew.text.world',
     defaultMessage: 'world'
   },
   NameTheKey: {
